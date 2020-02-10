c4feb49d4b304d8083c6152408715efb6c8cad37
Michel Dismer <m.dismer@velian.de>
2020-02-10 09:49:50 +0100

    update smooth-dnd

---
+++ docs/main.bcc33be695e627e071b8.bundle.js
@@ -0,0 +1,14 @@
+;(window.webpackJsonp = window.webpackJsonp || []).push([
+  [0],
+  {
+    298: function(n, o, p) {
+      p(299), (n.exports = p(321))
+    },
+    299: function(n, o, p) {
+      'use strict'
+      p.r(o)
+      p(300)
+    }
+  },
+  [[298, 1, 2]]
+])
